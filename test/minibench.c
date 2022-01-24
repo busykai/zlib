1c7dfb874b080d792308d196df1e8c8daaf0dd79
Arzhan Kinzhalin <arzhan.i.kinzhalin@intel.com>
2022-01-24 15:32:59 -0800

    Add support for compression levels.

--- test/minibench.c
+++ test/minibench.c
@@ -59,20 +59,49 @@ int main(argc, argv)
     int i;
     int times = 100;
     struct timespec start, end;
+    int64_t elapsed_nsec;
+    int opt;
+    char mode[4];
+    int forever = 0;
+
+    strcpy(mode, "wb6");
 
     prog = basename(argv[0]);
 
-    if (argc != 2) {
-        error("Must specify exactly one file to compress");
-        exit(1);
+    while ((opt = getopt(argc, argv, "123456789f")) != -1) {
+        switch (opt) {
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                fprintf(stderr, "Setting mode to: %c\n", opt);
+                mode[2] = opt;
+                break;
+            case 'f':
+                forever = 1;
+                break;
+            default:
+                fprintf(stderr, "Option %c not supported.\n", opt);
+                exit(EXIT_FAILURE);
+        }
     }
 
-    len = strlen(argv[1]);
+    if (optind >= argc) {
+        error("Must specify exactly one file to compress");
+        exit(EXIT_FAILURE);
+    }
+
+    len = strlen(argv[optind]);
     if (len > MAX_NAME_LEN) {
         fprintf(stderr, "Filename too long: %s\n", argv[1]);
         exit(1);
     }
-    strncpy(file, argv[1], len);
+    strncpy(file, argv[optind], len);
     file[len] = '\0';
 
     in = fopen(file, "rb");
@@ -80,7 +109,8 @@ int main(argc, argv)
         perror(file);
         exit(1);
     }
-    out = gzopen("/dev/null", "wb");
+
+    out = gzopen("/dev/null", mode);
     if (out == NULL) {
         fprintf(stderr, "%s: can't gzopen %s\n", prog, "/dev/null");
         exit(1);
@@ -89,7 +119,7 @@ int main(argc, argv)
     if (clock_gettime(CLOCK_MONOTONIC, &start))
         perror("clock_gettime");
 
-    for (i = 0; i < times; i++) {
+    for (i = 0; i < times || forever; i++) {
         for (;;) {
             len = (int)fread(buf, 1, sizeof(buf), in);
             if (ferror(in)) {
@@ -106,7 +136,7 @@ int main(argc, argv)
     if (clock_gettime(CLOCK_MONOTONIC, &end))
         perror("clock_gettime");
 
-    printf("Elapsed time: %.2f sec\n", elapsed(&start, &end) / 1000000000.0);
+    printf("Elapsed time (level %c): %d usec/op (%d ops in %.2f secs)\n", mode[2], elapsed(&start, &end) / times / 1000000, times, elapsed(&start, &end) / 1000000000.0);
 
     return 0;
 }
